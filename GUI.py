1b83e0b6d2d8d2f466c5637ba73a4cdcbc03981c
Đau Mắt <66857266+leducminh85@users.noreply.github.com>
2021-05-25 04:49:32 +0700

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -4,22 +4,95 @@ import tkinter as tk
 from PIL import ImageTk, Image
 from tkinter import filedialog
 
-TITLE = "Client"
+TITLE = "LOGIN"
 
-def DangNhap():
-    def __init__window():
-        Client_windows.title(80*" "+TITLE)
-        screen_width = Client_windows.winfo_screenwidth()
-        screen_height = Client_windows.winfo_screenheight()
-        x_cordinate = int((screen_width/2) - (600/2))
-        y_cordinate = int((screen_height/2) - (400/2))
-        Client_windows.geometry("{}x{}+{}+{}".format(600, 400, x_cordinate, y_cordinate))
+def PlaceWindow(window, window_width, window_height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x_cordinate = int((screen_width/2) - (window_width/2))
+    y_cordinate = int((screen_height/2) - (window_height/2))
+    window.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
 
-    __init__window()
+def CloseWindow(root):
+    root.destroy()
+
+def __init__window():
+   
+    def LoginFailed():
+        
+        RootLoginFailed = Toplevel(Client_windows)
+        RootLoginFailed.title(10*' '+"Lỗi đăng nhập")
+        PlaceWindow(RootLoginFailed, 300, 180)
+
+        FailLabel = Label(RootLoginFailed, text = "Tài khoản hoặc mật khẩu của bạn không chính xác!\n Vui lòng nhập lại")
+        FailLabel.place(x=10, y = 40)
+        OkButton = Button(RootLoginFailed, text = "OK",command = lambda: CloseWindow(RootLoginFailed))
+        OkButton.place(x = 85, y = 110, width = 130)
+
+    def Login(event):
+        print("Login")
+
+        #check
+        LoginFailed()
+
+    def Register(event):
+        print("Register")
+        RootRegister = Toplevel(Client_windows)
+        RootRegister.title(20*' '+"Đăng ký tài khoản")
+        PlaceWindow(RootRegister, 400, 200)
+
+        usernameLabel = Label(RootRegister, text = "Username: ")
+        usernameLabel.place(x = 30, y = 10)
+        usernameEntry = Entry(RootRegister)
+        usernameEntry.place(x= 130, y = 10, width = 200)
+
+        passwordLabel = Label(RootRegister, text = "Password: ")
+        passwordLabel.place(x = 30, y = 50)
+        passwordEntry = Entry(RootRegister)
+        passwordEntry.place(x= 130, y = 50, width = 200)
+
+        passwordLabel = Label(RootRegister, text = "Confirm\nPassword: ")
+        passwordLabel.place(x = 30, y = 90)
+        passwordEntry = Entry(RootRegister)
+        passwordEntry.place(x= 130, y = 90, width = 200)
+
+    #init window
+    Client_windows.title(80*" "+TITLE)
+    PlaceWindow(Client_windows, 600, 400)
+    Client_windows.resizable(0, 0)
+
+    #label Library
+    Label_Library = Text(Client_windows, bd = 0)
+    Label_Library.place(x = 200, y = 60, width = 200, height = 20)
+    Label_Library.insert('end',6*" "+"LIBRARY ONLINE")
+    Label_Library.configure(state='disabled')
+
+    #username va password
+    usernameLabel = Label(Client_windows, text = "Username: ")
+    usernameLabel.place(x = 100, y = 140)
+
+    usernameEntry = Entry(Client_windows)
+    usernameEntry.place(x= 200, y = 140, width = 200)
+
+    passwordLabel = Label(Client_windows, text = "Password: ")
+    passwordLabel.place(x = 100, y = 190)
+
+    passwordEntry = Entry(Client_windows)
+    passwordEntry.place(x= 200, y = 190, width = 200)
+
+    #Dang nhap, dang ki
+    LoginButton = Button(Client_windows, text = "Đăng nhập")
+    LoginButton.bind("<Button-1>",Login)
+    LoginButton.place(x = 320, y = 250, width = 80)
+
+    RegisterButton = Button(Client_windows, text = "Đăng ký")
+    RegisterButton.bind("<Button-1>",Register)
+    RegisterButton.place(x = 200, y = 250, width = 80)
+
+        
+
         
 Client_windows = Tk()
+__init__window()
 
-
-
-DangNhap()
 Client_windows.mainloop()
