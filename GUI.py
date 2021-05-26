3368db08142f14880359c7e38d484d94f018f64b
Đau Mắt <66857266+leducminh85@users.noreply.github.com>
2021-05-26 12:13:32 +0700

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
-from PIL import ImageTk, Image
 from tkinter import filedialog
+
 
 TITLE = "LOGIN"
 
@@ -16,12 +16,124 @@ def PlaceWindow(window, window_width, window_height):
 def CloseWindow(root):
     root.destroy()
 
-def __init__window():
+def Function():
+    def Logout():
+        print("Logout")
+        RootFunction.destroy()
+        main()
+
+    def Search():
+        print("Search")
+        
+    def SelectFunction(event):
+        def ReadBook():    
+            def Back():
+                CloseWindow(RootRead)
+                FunctionChoosen.set("Chọn chức năng") 
+
+            RootRead = Toplevel(RootFunction)
+            RootRead.grab_set()
+            RootRead.title(220*' '+"Đọc sách")
+            PlaceWindow(RootRead, 1500, 800)
+            RootRead.resizable(0, 0)
+
+            #BackToMenu
+            BackButton = Button(RootRead, text = "Trở lại", command = Back)
+            BackButton.place(x= 20, y = 20, width = 100)
+
+            #Input BookID
+            BookIDInput = Text(RootRead)
+            BookIDInput.insert('end',"Nhập ID sách....")
+            BookIDInput.place(x = 20, y = 70, width = 800, height = 20)
+
+            #Input ID Button
+            InputIDButton = Button(RootRead, text = "Xem sách")
+            InputIDButton.place(x= 830, y = 70, width = 200)
+        
+            #BookContent
+            BookContent = Text(RootRead)
+            BookContent.configure(state='disabled')
+            BookContent.place(x = 20, y = 110, width = 1450, height = 650)
+            vsb = ttk.Scrollbar(RootRead, command=BookContent.yview)
+            vsb.place(x = 1470, y = 110, height =650)
+
+
+
+        def DownloadBook():
+            print("Download book")
+        
+        s= FunctionChoosen.get()
+        if  s == " Đọc sách": ReadBook()
+        if  s == " Tải sách": DownloadBook()
+   
+
+    RootFunction = Tk()
+    RootFunction.title(220*' '+"MENU")
+    PlaceWindow(RootFunction, 1500, 700)
+    RootFunction.resizable(0, 0)
+
+    #Library Label
+    LibraryLabel = Text(RootFunction, bd = 0)
+    LibraryLabel.insert('end',80*" "+"ONLINE LIBRARY")
+    LibraryLabel.configure(state='disabled')
+    LibraryLabel.place(x = 20, y = 20, height = 20, width = 1460)
+
+    #HelloLabel
+    userName = ".........."  #insert userName here
+    HelloLabel = Label(RootFunction, text = "Xin chào " + userName)
+    HelloLabel.place(x = 1200-userName.__len__(), y = 50)
+
+    #LogoutButton
+    SearchButton = Button(RootFunction, text ="Đăng xuất", command = Logout)
+    SearchButton.place(x=1300, y = 50, width = 180)
+    
+    #ChooseFunction
+    FunctionChoosen = ttk.Combobox(RootFunction,state="readonly")
+    FunctionChoosen.set("Chọn chức năng") 
+    FunctionChoosen['values'] = (' Đọc sách',' Tải sách')
+    FunctionChoosen.place(x = 20, y = 60, width = 200)
+    FunctionChoosen.bind('<<ComboboxSelected>>', SelectFunction)
+
+    #SearchBook
+    SearchBox = Text(RootFunction)
+    SearchBox.insert('end',"Tra cứu tại đây...")
+    SearchBox.place(x = 20, y = 100, height = 20, width = 1200)
+
+    #SearchButton
+    SearchButton = Button(RootFunction, text ="Tra cứu", command = Search)
+    SearchButton.place(x=1250, y = 100, width = 230)
+
+
+    #TableBook
+    tree =ttk.Treeview(RootFunction, column=("c1", "c2", "c3","c4","c5","c6"), show='headings')
+    vsb = ttk.Scrollbar(RootFunction, orient="vertical", command=tree.yview)
+
+    tree.bind('<Button-1>', "break")
+   
+    tree.column("#1", anchor=tk.CENTER, minwidth=0, width=50, stretch= FALSE)
+    tree.heading("#1", text="STT")
+    tree.column("#2", anchor=tk.CENTER, minwidth=0, width=450 ,stretch=FALSE)
+    tree.heading("#2", text="Tên sách")
+    tree.column("#3", anchor=tk.CENTER, minwidth=0, width=150, stretch=FALSE)
+    tree.heading("#3", text="ID")
+    tree.column("#4", anchor=tk.CENTER, minwidth=0, width=350, stretch=FALSE)
+    tree.heading("#4", text="Tác giả")
+    tree.column("#5", anchor=tk.CENTER, minwidth=0, width=300, stretch=FALSE)
+    tree.heading("#5", text="Loại sách")
+    tree.column("#6", anchor=tk.CENTER, minwidth=0, width=140, stretch=FALSE)
+    tree.heading("#6", text="Năm sáng tác")
+    tree.place(x = 20, y = 150, height = 520)
+    vsb.place(x=20+50+450+150+350+300+140, y=150, height=520)
+    
+    
+def __init__window(Client_windows):
     def Login(event):
         def LoginCheck():
             #check
-            LoginFail()
-
+            #LoginFail()
+            LoginSuccessful()
+            
+            
         def LoginFail():
             RootLoginFail = Toplevel(Client_windows)
             RootLoginFail.grab_set()
@@ -35,15 +147,25 @@ def __init__window():
             OkButton.place(x = 85, y = 110, width = 130)
 
         def LoginSuccessful():
-            pass
+            RootLoginS = Toplevel(Client_windows)
+            RootLoginS.grab_set()
+
+            RootLoginS.title(10*' '+"Đăng nhập")
+            PlaceWindow(RootLoginS, 300, 180)
+
+            FailLabel = Label(RootLoginS, text = "Đăng nhập thành công! \n Nhấn OK để tiếp tục")
+            FailLabel.place(x= 90, y = 40)
+            OkButton = Button(RootLoginS, text = "OK",command = lambda: [CloseWindow(Client_windows),Function()])
+            OkButton.place(x = 85, y = 110, width = 130)
+            
         
         LoginCheck()
 
     def Register(event):
         def checkRegistry():
             #check
-            #RegisterSuccessful()
-            RegisterFail()
+            RegisterSuccessful()
+            #RegisterFail()
 
         def RegisterSuccessful():
             RootRS = Toplevel(RootRegister)
@@ -88,6 +210,9 @@ def __init__window():
         OkButton = Button(RootRegister, text = "OK",command = checkRegistry)
         OkButton.place(x = 140, y = 95, width = 130)
 
+    def End(event):
+        CloseWindow(Client_windows)
+
     #init window
     Client_windows.title(80*" "+TITLE)
     PlaceWindow(Client_windows, 600, 400)
@@ -96,7 +221,7 @@ def __init__window():
     #label Library
     Label_Library = Text(Client_windows, bd = 0)
     Label_Library.place(x = 200, y = 60, width = 200, height = 20)
-    Label_Library.insert('end',6*" "+"LIBRARY ONLINE")
+    Label_Library.insert('end',6*" "+"ONLINE LIBRARY")
     Label_Library.configure(state='disabled')
 
     #username va password
@@ -121,10 +246,13 @@ def __init__window():
     RegisterButton.bind("<Button-1>",Register)
     RegisterButton.place(x = 200, y = 250, width = 80)
 
-        
+    EndButton = Button(Client_windows, text = "Kết thúc")
+    EndButton.bind("<Button-1>", End)
+    EndButton.place(x = 200, y = 300, width = 200)    
 
-        
-Client_windows = Tk()
-__init__window()
+def main():     
+    Client_windows = Tk()
+    __init__window(Client_windows)
+    Client_windows.mainloop()
 
-Client_windows.mainloop()
+main()
